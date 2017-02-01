629799d2787e59f2fd4ae27c06070d7b03cef2d2
rechen <rechen@qq.com>
2017-02-01 17:46:14 +0800

    first commit by wjluo

---
+++ src/com/cmbchina/hcmp/cloudlabs/test1.java
@@ -0,0 +1,11 @@
+package com.cmbchina.hcmp.cloudlabs;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		system.println("hello.")
+
+	}
+
+}

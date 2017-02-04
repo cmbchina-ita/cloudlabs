dfe7f8ceb418bdae4c758487b3d5bac13be2cf73
rechen <rechen@qq.com>
2017-02-04 11:13:54 +0800

    test1

--- src/com/cmbchina/hcmp/cloudlabs/test1.java
+++ src/com/cmbchina/hcmp/cloudlabs/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		system.println("hello. world, 罗文江")
+		System.out.print("hello. world, 罗文江");
 
 	}
 
